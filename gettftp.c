f79cdad6933aab58eb3017825df784c0a20dd3b4
llicecream <lucieshli@gmail.com>
2023-12-11 12:06:02 +0100

    re com gettftp

--- gettftp.c
+++ gettftp.c
@@ -8,7 +8,7 @@
 
 #define MAX_BUFFER_SIZE 516
 
-void create_rrq_packets(char *filename,char *mode,char *request_packet){
+void create_rrq_packets(char *filename,char *mode,char *request_packet){ // read request
     request_packet[0]=0;
     request_packet[1]=1;
     strcpy(request_packet+2,filename);
@@ -23,11 +23,13 @@ void tftp_client(char *server_ip,int port,char *filename){
     hints.ai_family=AF_INET;
     hints.ai_socktype=SOCK_DGRAM;
 
+    // resolution of the server address
     if (getaddrinfo(server_ip,NULL,&hints,&res)!=0){
         perror("Error in the resolution of the server address");
         exit(EXIT_FAILURE);
     }
 
+    // Socket creation
     sockfd=socket(res->ai_family,res->ai_socktype,res->ai_protocol);
     if (sockfd<0){
         perror("Error in the creation of the socket");
@@ -42,6 +44,7 @@ void tftp_client(char *server_ip,int port,char *filename){
     char request_packet[MAX_BUFFER_SIZE];
     create_rrq_packets(filename,"octet",request_packet);
 
+    // On error for "sendto", -1 is returned, we check if there is an error.
     if (sendto(sockfd,request_packet,MAX_BUFFER_SIZE,0,(struct sockaddr *)&server_addr, sizeof(server_addr))==-1){
         perror("Error sending rrq request");
         close(sockfd);
@@ -49,7 +52,7 @@ void tftp_client(char *server_ip,int port,char *filename){
     }
 }
 
-int main(int argc,char *argv[]){
+int main(int argc,char *argv[]){  // Processing command line arguments
     if(argc==4){  // gettftp ; server ; port; file
         char *server_ip=argv[1];
         if (atoi(argv[2])==0){  // if there isnt the port, return failure
@@ -61,7 +64,7 @@ int main(int argc,char *argv[]){
         char *filename = argv[3];
     } else if(argc==3){
         char *server_ip=argv[1];
-        int port = 69;
+        int port = 69;  // default value
         char *filename = argv[2];
     } else {
         fprintf(stderr,"Usage: %s <server_ip> <port> <filename>\n",argv[0]);
